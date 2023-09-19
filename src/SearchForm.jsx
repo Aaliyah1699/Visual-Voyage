727ec8a981d647359c94db899fdf1d40d1c0c241
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-09-18 21:14:00 -0500

    setup search form

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,5 +1,22 @@
+// import { useGlobalContext } from './context';
+
 const SearchForm = () => {
-    return <h4>SearchForm</h4>;
+    return (
+        <section>
+            <h1>Visual Voyage</h1>
+            <form className='search-form'>
+                <input
+                    type='text'
+                    className='form-input search-input'
+                    name='search'
+                    placeholder='flower'
+                />
+                <button type='submit' className='btn'>
+                    Search
+                </button>
+            </form>
+        </section>
+    );
 };
 
 export default SearchForm;
